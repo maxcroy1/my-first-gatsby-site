0f927ad4c53babcdd010ae3258f6e165f6ecda5f
maxcroy1 <max.croy1@gmail.com>
2021-08-23 15:15:09 -0400

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => {
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
         alt="This is a dope ass whale."
-        src="https://lh3.googleusercontent.com/4eKGcDIpzGTPHebu_WyR-Ak-4UKhXhR4oOCLltrZ5lp73CLS0bl281joy6pY6R-_q7-uWRWMrTVC4qcOKJvkTM0zQ43UeiFsk_j-59E=w600"
+        src="../images/WHALE2.png"
       />
     </Layout>
   )
