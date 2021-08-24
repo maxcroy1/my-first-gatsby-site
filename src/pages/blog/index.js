c0c07ec7a03bf17250b15afbd60cd3af3b9bf9aa
maxcroy1 <max.croy1@gmail.com>
2021-08-23 21:00:25 -0400

    Finished Gatsby Tutorial Part 6

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,6 @@
 import * as React from 'react'
-import Layout from '../components/layout'
-import { graphql } from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
 
 const BlogPage = ({ data }) => {
     return (
@@ -9,11 +8,12 @@ const BlogPage = ({ data }) => {
             {
                 data.allMdx.nodes.map(node => (
                     <article key={node.id}>
-                        <h1>{node.frontmatter.title}</h1>
+                        <h2>
+                            <Link to={`/blog/${node.slug}`}>
+                                {node.frontmatter.title}
+                            </Link>
+                        </h2>
                         <p>Posted: {node.frontmatter.date}</p>
-                        <MDXRenderer>
-                            {node.body}
-                        </MDXRenderer>
                     </article>
                 ))
             }
@@ -30,7 +30,7 @@ export const query = graphql`
                     title
                 }
                 id
-                body
+                slug
             }
         }
     }
